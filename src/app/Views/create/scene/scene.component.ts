a6b40f96055f1b22758750e72526338d93133ff1
William <wfreem2@gmail.com>
2022-10-16 17:42:19 -0400

    Changed color-secondary, fixed spacing in create component

--- src/app/Views/create/scene/scene.component.ts
+++ src/app/Views/create/scene/scene.component.ts
@@ -10,6 +10,7 @@ export class SceneComponent implements OnInit {
 
 
   selectedSetting: selectedSetting = 'geo'
+  
   @Input() scene!:Scene
   @Output() sceneSavedEvent = new EventEmitter<Scene>()
 
@@ -17,6 +18,16 @@ export class SceneComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  clear(): void{
+    this.scene.geoPos = {
+       alt: 0,
+       lat: 0,
+       long: 0
+    }
+
+    this.scene.title = ''
+  }
+
 }
 
-type selectedSetting = 'geo' | 'settings'
+type selectedSetting = 'geo' | 'script'
