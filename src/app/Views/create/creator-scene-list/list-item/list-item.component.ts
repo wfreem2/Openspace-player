a70543ae5b52715ddf13a1dfaa6d5621d27882c7
wfreem <wfreem2@gmail.com>
2022-10-31 15:49:55 -0400

    Added context menu to creator-scene-list items

--- src/app/Views/create/creator-scene-list/list-item/list-item.component.ts
+++ src/app/Views/create/creator-scene-list/list-item/list-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { Scene } from 'src/app/Interfaces/Scene';
+import { isChildClicked } from 'src/app/Utils/utils';
 
 @Component({
   selector: 'list-item',
@@ -8,13 +9,33 @@ import { Scene } from 'src/app/Interfaces/Scene';
 })
 export class ListItemComponent implements OnInit {
 
+  @ViewChild('m') more!: ElementRef
+
   @Input() isActive: boolean = false
   @Input() scene!: Scene
 
   @Output() itemClickedEvent = new EventEmitter<ListItemComponent>()
+  @Output() deleteClickedEvent = new EventEmitter<ListItemComponent>()
+
+  isCtxShowing: boolean = false
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', this.ctxMenuClicked.bind(this))
+  } 
   
-  constructor() { } 
 
   ngOnInit(): void { }
 
+  private ctxMenuClicked(event: Event){
+    const isMoreClicked = isChildClicked(this.more.nativeElement, event.target)
+
+    if(!isMoreClicked){
+      this.isCtxShowing = false
+    }
+
+  } 
+
+  onDeleteClicked(): void{
+    this.deleteClickedEvent.emit(this)
+  }
 }
